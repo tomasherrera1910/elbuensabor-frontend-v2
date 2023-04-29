4b750de860f26c87d9be9072a3606b284ee1cd26
tomasherrera1910 <tomymza10@gmail.com>
2023-04-29 20:43:48 -0300

    custom head and using in index

---
+++ components/CustomHead.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function CustomHead ({ section }: { section: string }) {
+  return (
+    <Head>
+      <title>El Buen Sabor | {section}</title>
+      <meta name='description' content='Final project UTN rework' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from 'next/head'
 import Login from '@/components/Login'
+import CustomHead from '@/components/CustomHead'
 
 export default function Home () {
+  const user = null
   return (
     <>
-      <Head>
-        <title>El Buen Sabor</title>
-        <meta name='description' content='Final project UTN rework' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+      <CustomHead section={`${user != null ? 'Home' : 'Login'}`} />
       <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
         <Login />
       </div>
