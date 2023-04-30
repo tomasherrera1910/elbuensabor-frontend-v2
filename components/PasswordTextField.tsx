bb2f753d314ed92e0cad0ef65f893e6baa55c7b9
tomasherrera1910 <tomymza10@gmail.com>
2023-04-30 13:23:38 -0300

    add formik neccesary props for show errors

--- components/PasswordTextField.tsx
+++ components/PasswordTextField.tsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 import { TextField, InputAdornment, IconButton } from '@mui/material'
 import { VisibilityOff, Visibility } from '@mui/icons-material'
-export default function PasswordTextField ({ name, label }: { name: string, label: string }) {
+
+interface Props {
+  name: string
+  label: string
+  value: string
+  handleChange: any
+  handleBlur: any
+  color: 'primary' | 'error'
+  error?: boolean
+  helperText?: string | false
+}
+export default function PasswordTextField ({ name, label, value, handleChange, handleBlur, color, error, helperText }: Props) {
   const [showPassword, setShowPassword] = useState(false)
   return (
     <TextField
-      variant='standard' label={label} name={name} required
+      variant='standard' label={label} name={name} value={value}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      color={color}
+      error={error}
+      helperText={helperText}
       type={showPassword ? 'text' : 'password'}
+      required
       InputProps={{
         endAdornment: (
           <InputAdornment position='end'>
