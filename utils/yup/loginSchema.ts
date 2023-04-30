567ffc9b98c10e66cef5e1cfaaa5fb3891c41724
tomasherrera1910 <tomymza10@gmail.com>
2023-04-30 13:23:48 -0300

    login schema for validations

---
+++ utils/yup/loginSchema.ts
@@ -0,0 +1,8 @@
+import * as Yup from 'yup'
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string().email('Ingrese un email válido').required('Ingrese su email'),
+  password: Yup.string().required('Ingrese su contraseña')
+})
+
+export default loginSchema
