e99677dc1129d017fe816c304ba441cd6514e585
tomasherrera1910 <tomymza10@gmail.com>
2023-05-01 10:45:04 -0300

    add post actions

---
+++ utils/CRUDActions.ts
@@ -0,0 +1,21 @@
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3001'
+
+export function getInfo () {
+  return null
+}
+export async function postInfo (path: string, token?: string, body?: any) {
+  const url = `${BASE_URL}/${path}`
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      Authorization: `Bearer ${token ?? ''}`
+    },
+    body: JSON.stringify(body)
+  })
+  if (!response.ok) {
+    return console.error(response)
+  }
+  const data = await response.json()
+  return data
+}
