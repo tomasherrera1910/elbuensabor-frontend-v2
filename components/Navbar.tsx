9977a8a4bc79e11a032ba3f7d0cede572121de7a
tomasherrera1910 <tomymza10@gmail.com>
2023-05-03 11:51:25 -0300

    start sidebar and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,18 @@
 import { useUserSession } from '@/store/user'
 import { AccountCircle, Fastfood, ShoppingCart } from '@mui/icons-material'
-import { AppBar, IconButton, Stack, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material'
+import Link from 'next/link'
 
 export default function Navbar () {
   const userInfo = useUserSession(state => state.userInfo)
   return (
-    <AppBar>
-      <Toolbar sx={{ justifyContent: 'space-around' }}>
-        <Stack direction='row' gap={1} alignItems='center'>
-          <Fastfood />
-          <Typography border='GrayText'>EL BUEN SABOR</Typography>
-        </Stack>
+    <AppBar position='fixed'>
+      <Toolbar sx={{ justifyContent: { xs: 'space-between', sm: 'space-around' } }}>
+        <Link href='/'>
+          <Button startIcon={<Fastfood />} color='inherit'>
+            EL BUEN SABOR
+          </Button>
+        </Link>
         <Stack direction='row' gap={2}>
           <Stack direction='row' alignItems='center' gap={1} sx={{ display: { xs: 'none', sm: 'flex' } }}>
             <AccountCircle />
