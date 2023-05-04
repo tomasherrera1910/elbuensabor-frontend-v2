af507f984d83dca76c34c807b9bf9518d32a2a9b
tomasherrera1910 <tomymza10@gmail.com>
2023-05-04 19:08:25 -0300

    store for sensitive information that should not be stored in localStorage

---
+++ store/userSensitivityInfo.ts
@@ -0,0 +1,17 @@
+import { getInfo } from '@/utils/CRUDActions'
+import { User } from '@/utils/types'
+import { create } from 'zustand'
+
+interface State {
+  user?: User
+  getProfile: (token: string) => Promise<void>
+}
+export const useUserSensivityInfo = create<State>((set) => {
+  return {
+    user: undefined,
+    getProfile: async (token: string) => {
+      const loggedUser = await getInfo('users/profile', token)
+      set({ user: loggedUser })
+    }
+  }
+})
