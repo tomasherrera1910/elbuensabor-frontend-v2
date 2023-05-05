60195779d24d623bba6dec41131067db10da5e7e
tomasherrera1910 <tomymza10@gmail.com>
2023-05-05 11:33:44 -0300

    dinamic sidebar depend of screen size

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { useUserAllInfo } from '@/store/userAllInfo'
-import { FoodBank, Logout, LunchDining, OtherHouses, SupervisorAccount } from '@mui/icons-material'
-import { Divider, Drawer, Stack, useTheme } from '@mui/material'
+import { Close, FoodBank, Logout, LunchDining, OtherHouses, SupervisorAccount } from '@mui/icons-material'
+import { Divider, Drawer, IconButton, Stack, useMediaQuery, useTheme } from '@mui/material'
 import SectionButton from './SectionButton'
 
 const menuSections = [
@@ -21,11 +21,18 @@ const menuSections = [
   }
 ]
 
-export default function Sidebar () {
+export default function Sidebar ({ open, handleClose }: { open: boolean, handleClose?: () => void }) {
   const user = useUserAllInfo(state => state.user)
   const theme = useTheme()
+  const smallScreen = useMediaQuery(theme.breakpoints.down('sm'))
   return (
-    <Drawer variant='persistent' open PaperProps={{ sx: { backgroundColor: theme.palette.primary.main, color: '#fff' } }}>
+    <Drawer variant={smallScreen ? 'temporary' : 'permanent'} open={open} PaperProps={{ sx: { backgroundColor: theme.palette.primary.main, color: '#fff', paddingTop: { sm: 8 }, width: { xs: '100vw', sm: 'auto' } } }}>
+      {smallScreen && (
+        <>
+          <SectionButton icon={<Close sx={{ color: '#fff' }} />} title='CERRAR MENÚ' handleClick={handleClose} />
+          <Divider />
+        </>
+      )}
       {user?.rol === 'admin' && <SectionButton icon={<SupervisorAccount sx={{ color: '#fff' }} />} title='ADMIN' />}
       <Divider />
       <Stack sx={{ flexGrow: 1 }}>
