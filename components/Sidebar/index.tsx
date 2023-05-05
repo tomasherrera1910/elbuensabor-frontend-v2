44902498ec958a7a8244c07e2457e2a69546606e
tomasherrera1910 <tomymza10@gmail.com>
2023-05-05 19:36:49 -0300

    improving responsive and sidebar position in large screens

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { useUserAllInfo } from '@/store/userAllInfo'
 import { Close, FoodBank, Logout, LunchDining, OtherHouses, SupervisorAccount } from '@mui/icons-material'
-import { Divider, Drawer, IconButton, Stack, useMediaQuery, useTheme } from '@mui/material'
+import { Container, Divider, Drawer, Stack, useMediaQuery, useTheme } from '@mui/material'
 import SectionButton from './SectionButton'
 
 const menuSections = [
@@ -24,24 +24,26 @@ const menuSections = [
 export default function Sidebar ({ open, handleClose }: { open: boolean, handleClose?: () => void }) {
   const user = useUserAllInfo(state => state.user)
   const theme = useTheme()
-  const smallScreen = useMediaQuery(theme.breakpoints.down('sm'))
+  const smallScreen = useMediaQuery(theme.breakpoints.down('md'))
   return (
-    <Drawer variant={smallScreen ? 'temporary' : 'permanent'} open={open} PaperProps={{ sx: { backgroundColor: theme.palette.primary.main, color: '#fff', paddingTop: { sm: 8 }, width: { xs: '100vw', sm: 'auto' } } }}>
-      {smallScreen && (
-        <>
-          <SectionButton icon={<Close sx={{ color: '#fff' }} />} title='CERRAR MENÚ' handleClick={handleClose} />
-          <Divider />
-        </>
-      )}
-      {user?.rol === 'admin' && <SectionButton icon={<SupervisorAccount sx={{ color: '#fff' }} />} title='ADMIN' />}
-      <Divider />
-      <Stack sx={{ flexGrow: 1 }}>
-        {menuSections.map((section) => (
-          <SectionButton icon={section.icon} title={section.title} key={section.title} />
-        ))}
-      </Stack>
-      <Divider />
-      <SectionButton icon={<Logout sx={{ color: '#fff' }} />} title='CERRAR SESIÓN' />
-    </Drawer>
+    <Container sx={{ display: { xs: 'none', md: 'flex' }, width: 240, height: '100vh' }}>
+      <Drawer variant={smallScreen ? 'temporary' : 'permanent'} open={open} onClose={handleClose} PaperProps={{ sx: { backgroundColor: theme.palette.primary.main, color: '#fff', paddingTop: { md: 8 }, width: { xs: '100vw', sm: 'auto' }, position: 'fixed' } }}>
+        {smallScreen && (
+          <>
+            <SectionButton icon={<Close sx={{ color: '#fff' }} />} title='CERRAR MENÚ' handleClick={handleClose} />
+            <Divider />
+          </>
+        )}
+        {user?.rol === 'admin' && <SectionButton icon={<SupervisorAccount sx={{ color: '#fff' }} />} title='ADMIN' />}
+        <Divider />
+        <Stack sx={{ flexGrow: 1 }}>
+          {menuSections.map((section) => (
+            <SectionButton icon={section.icon} title={section.title} key={section.title} />
+          ))}
+        </Stack>
+        <Divider />
+        <SectionButton icon={<Logout sx={{ color: '#fff' }} />} title='CERRAR SESIÓN' />
+      </Drawer>
+    </Container>
   )
 }
