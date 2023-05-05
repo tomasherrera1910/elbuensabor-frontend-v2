12801c5a3d803800832cfa8c1a092b5267de195e
tomasherrera1910 <tomymza10@gmail.com>
2023-05-05 19:36:03 -0300

    Now show correctly sidebar and main content

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,21 @@ import CustomHead from '@/components/CustomHead'
 import Navbar from '@/components/Navbar'
 import useUser from '@/hooks/useUser'
 import LoadingPage from '@/components/LoadingPage'
+import { Stack } from '@mui/material'
 
 export default function Home () {
   const { user, loading, userSession } = useUser()
   return (
     <>
       <CustomHead section={`${loading ? 'Cargando' : user ? 'Home' : 'Login'}`} />
-      {userSession && !loading && <Navbar />}
-      <main style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-        {loading && <LoadingPage />}
-        {user && userSession && !loading && <HomeComponent />}
-        {!userSession && !loading && <Login />}
-      </main>
+      <Stack flexDirection='row'>
+        {userSession && !loading && <Navbar />}
+        <main style={{ minHeight: '100vh', flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          {loading && <LoadingPage />}
+          {user && userSession && !loading && <HomeComponent />}
+          {!userSession && !loading && <Login />}
+        </main>
+      </Stack>
     </>
   )
 }
