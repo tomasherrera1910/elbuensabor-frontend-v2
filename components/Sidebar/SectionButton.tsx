6f104fc3b55cb3d6a5abf618f478ae89ba6e3d7f
tomasherrera1910 <tomymza10@gmail.com>
2023-05-05 11:34:05 -0300

    add handleClick

--- components/Sidebar/SectionButton.tsx
+++ components/Sidebar/SectionButton.tsx
@@ -1,9 +1,9 @@
 import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 
-export default function SectionButton ({ icon, title }: { icon: any, title: string }) {
+export default function SectionButton ({ icon, title, handleClick }: { icon: any, title: string, handleClick?: () => void }) {
   return (
     <ListItem>
-      <ListItemButton>
+      <ListItemButton onClick={handleClick}>
         <ListItemIcon sx={{ display: 'flex', justifyContent: 'center' }}>
           {icon}
         </ListItemIcon>
