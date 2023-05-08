5973dd2d78c74fdd06ff098984f2d724f144d54e
tomasherrera1910 <tomymza10@gmail.com>
2023-05-08 12:42:02 -0300

    fix confirmLogout with redirect

--- components/Sidebar/ConfirmLogout.tsx
+++ components/Sidebar/ConfirmLogout.tsx
@@ -1,8 +1,14 @@
 import { useUserSession } from '@/store/user'
 import { Button, Dialog, DialogActions, DialogTitle } from '@mui/material'
+import { useRouter } from 'next/router'
 
 export default function ConfirmLogout ({ showModal, handleShowModal }: { showModal: boolean, handleShowModal: () => void }) {
   const logout = useUserSession(state => state.logout)
+  const router = useRouter()
+  const handleLogout = async () => {
+    logout()
+    await router.replace('/')
+  }
   return (
     <Dialog
       open={showModal}
@@ -16,7 +22,7 @@ export default function ConfirmLogout ({ showModal, handleShowModal }: { showMod
         <Button autoFocus onClick={handleShowModal}>
           NO
         </Button>
-        <Button onClick={logout} autoFocus>
+        <Button onClick={handleLogout} autoFocus>
           SI
         </Button>
       </DialogActions>
