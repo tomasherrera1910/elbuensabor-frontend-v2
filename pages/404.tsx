21a7ef789c6b5593a7a3890ec5b4edc47f0fe169
tomasherrera1910 <tomymza10@gmail.com>
2023-05-09 00:06:28 -0300

    default 404 styled with layout if your session is active

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,24 @@
+import CustomHead from '@/components/CustomHead'
+import Layout from '@/components/Layout'
 import NotFound from '@/components/NotFound'
+import useUser from '@/hooks/useUser'
 
 export default function Custom404 () {
+  const { user } = useUser()
   return (
-    <NotFound />
+    <>
+      <CustomHead section='404' />
+      {user
+        ? (
+          <Layout>
+            <NotFound />
+          </Layout>
+          )
+        : (
+          <main style={{ minHeight: '100vh', display: 'flex', alignItems: 'center' }}>
+            <NotFound />
+          </main>
+          )}
+    </>
   )
 }
