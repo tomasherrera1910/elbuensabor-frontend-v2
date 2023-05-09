327f00f7f06d0d8ff8dab47c98a7a3999fd19d75
tomasherrera1910 <tomymza10@gmail.com>
2023-05-09 12:57:40 -0300

    start admin panel

--- components/Admin/index.tsx
+++ components/Admin/index.tsx
@@ -1,9 +1,49 @@
-import { Container, Typography } from '@mui/material'
+import { AccessibilityNew, Egg, EmojiEvents, Groups3, LunchDining, MonetizationOn, Money } from '@mui/icons-material'
+import { Button, Container, Divider, Stack, Typography } from '@mui/material'
 
+const MENU_OPTIONS = [
+  {
+    title: 'Asignación de roles de usuario',
+    icon: <Groups3 />
+  },
+  {
+    title: 'Artículos Insumo',
+    icon: <Egg />
+  },
+  {
+    title: 'Platos',
+    icon: <LunchDining />
+  },
+  {
+    title: 'Más Pedidos',
+    icon: <EmojiEvents />
+  },
+  {
+    title: 'Clientes frecuentes',
+    icon: <AccessibilityNew />
+  },
+  {
+    title: 'Ingresos',
+    icon: <MonetizationOn />
+  },
+  {
+    title: 'Ganancias',
+    icon: <Money />
+  }
+]
 export default function AdminMenu () {
   return (
-    <Container>
-      <Typography>ADMIN PAGE</Typography>
+    <Container maxWidth='md'>
+      <Typography variant='h5' component='h1' fontWeight='900' textAlign='center' marginBottom={4}>
+        PANEL DE ADMINISTRADOR
+      </Typography>
+      <Stack justifyContent='center' alignItems='center' gap={2} divider={<Divider orientation='horizontal' light variant='middle' flexItem />}>
+        {MENU_OPTIONS.map(option => (
+          <Button key={option.title} startIcon={option.icon} size='large' variant='outlined' sx={{ width: '100%' }}>
+            {option.title}
+          </Button>
+        ))}
+      </Stack>
     </Container>
   )
 }
