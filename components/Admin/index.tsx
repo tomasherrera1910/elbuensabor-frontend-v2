57c89a8ace435442fcdf35fd3925b458575b1dbb
tomasherrera1910 <tomymza10@gmail.com>
2023-05-10 00:03:04 -0300

    add links and paths

--- components/Admin/index.tsx
+++ components/Admin/index.tsx
@@ -1,47 +1,57 @@
 import { AccessibilityNew, Egg, EmojiEvents, Groups3, LunchDining, MonetizationOn, Money } from '@mui/icons-material'
 import { Button, Container, Divider, Stack, Typography } from '@mui/material'
+import Link from 'next/link'
 
 const MENU_OPTIONS = [
   {
     title: 'Asignación de roles de usuario',
-    icon: <Groups3 />
+    icon: <Groups3 />,
+    path: '/admin/roleManager'
   },
   {
     title: 'Artículos Insumo',
-    icon: <Egg />
+    icon: <Egg />,
+    path: '/admin/'
   },
   {
     title: 'Platos',
-    icon: <LunchDining />
+    icon: <LunchDining />,
+    path: '/admin/'
   },
   {
     title: 'Más Pedidos',
-    icon: <EmojiEvents />
+    icon: <EmojiEvents />,
+    path: '/admin/'
   },
   {
     title: 'Clientes frecuentes',
-    icon: <AccessibilityNew />
+    icon: <AccessibilityNew />,
+    path: '/admin/'
   },
   {
     title: 'Ingresos',
-    icon: <MonetizationOn />
+    icon: <MonetizationOn />,
+    path: '/admin/'
   },
   {
     title: 'Ganancias',
-    icon: <Money />
+    icon: <Money />,
+    path: '/admin/'
   }
 ]
 export default function AdminMenu () {
   return (
-    <Container maxWidth='md'>
+    <Container maxWidth='sm'>
       <Typography variant='h5' component='h1' fontWeight='900' textAlign='center' marginBottom={4}>
         PANEL DE ADMINISTRADOR
       </Typography>
       <Stack justifyContent='center' alignItems='center' gap={2} divider={<Divider orientation='horizontal' light variant='middle' flexItem />}>
         {MENU_OPTIONS.map(option => (
-          <Button key={option.title} startIcon={option.icon} size='large' variant='outlined' sx={{ width: '100%' }}>
-            {option.title}
-          </Button>
+          <Link href={option.path} key={option.title} style={{ width: '100%' }}>
+            <Button startIcon={option.icon} size='large' variant='outlined' sx={{ width: '100%' }}>
+              {option.title}
+            </Button>
+          </Link>
         ))}
       </Stack>
     </Container>
