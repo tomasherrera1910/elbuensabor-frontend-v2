04653cc4f9a958d48f06a70fd577cb6ebd85ef90
tomasherrera1910 <tomymza10@gmail.com>
2023-05-11 09:19:10 -0300

    add put function

--- utils/CRUDActions.ts
+++ utils/CRUDActions.ts
@@ -33,3 +33,21 @@ export async function postInfo (path: string, token?: string, body?: any) {
   const data = await response.json()
   return data
 }
+
+export async function editInfo (path: string, token?: string, body?: any) {
+  const url = `${BASE_URL}/${path}`
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      Authorization: `Bearer ${token ?? ''}`
+    },
+    body: JSON.stringify(body)
+  })
+  if (!response.ok) {
+    const errorMessage = await response.text()
+    throw new Error(errorMessage)
+  }
+  const data = await response.json()
+  return data
+}
