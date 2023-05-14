a1acd1e990b6314174597274fc06f6adb51f30d1
tomasherrera1910 <tomymza10@gmail.com>
2023-05-14 15:25:44 -0300

    using useSupplies hook

--- components/Admin/SuppliesManager/index.tsx
+++ components/Admin/SuppliesManager/index.tsx
@@ -3,9 +3,11 @@ import { Button, Container, Typography } from '@mui/material'
 import FormModal from './FormModal'
 import useToggle from '@/hooks/useToggle'
 import { Add } from '@mui/icons-material'
+import useSupplies from '@/hooks/useSupplies'
 
 export default function SuppliesManager ({ initialSupplies }: { initialSupplies: ItemSupply[] }) {
   const [showModalForm, handleModalForm] = useToggle()
+  const { supplies, addNewSupply } = useSupplies({ initialSupplies })
   return (
     <Container maxWidth='md'>
       <Typography variant='h5' component='h1' fontWeight='900' textAlign='center' marginBottom={4}>
@@ -14,8 +16,11 @@ export default function SuppliesManager ({ initialSupplies }: { initialSupplies:
       <Button startIcon={<Add />} onClick={handleModalForm}>
         Agregar Artículo
       </Button>
-      <FormModal open={showModalForm} handleClose={handleModalForm} />
-      {!initialSupplies?.length && <Typography textAlign='center'>No hay artículos aún!</Typography>}
+      <FormModal open={showModalForm} addNewSupply={addNewSupply} handleClose={handleModalForm} />
+      {!supplies?.length && <Typography textAlign='center'>No hay artículos aún!</Typography>}
+      {supplies.map(supply => (
+        <Typography key={supply.id}>{supply.denominacion}</Typography>
+      ))}
     </Container>
   )
 }
