ac48dc3eff21a5be84930e323c7d9eae81450b94
tomasherrera1910 <tomymza10@gmail.com>
2023-05-14 14:48:46 -0300

    feat button for open form and formModal component

--- components/Admin/SuppliesManager/index.tsx
+++ components/Admin/SuppliesManager/index.tsx
@@ -1,12 +1,20 @@
 import { ItemSupply } from '@/utils/types'
-import { Container, Typography } from '@mui/material'
+import { Button, Container, Typography } from '@mui/material'
+import FormModal from './FormModal'
+import useToggle from '@/hooks/useToggle'
+import { Add } from '@mui/icons-material'
 
 export default function SuppliesManager ({ initialSupplies }: { initialSupplies: ItemSupply[] }) {
+  const [showModalForm, handleModalForm] = useToggle()
   return (
     <Container maxWidth='md'>
       <Typography variant='h5' component='h1' fontWeight='900' textAlign='center' marginBottom={4}>
         ARTÍCULOS INSUMOS
       </Typography>
+      <Button startIcon={<Add />} onClick={handleModalForm}>
+        Agregar Artículo
+      </Button>
+      <FormModal open={showModalForm} handleClose={handleModalForm} />
       {!initialSupplies?.length && <Typography textAlign='center'>No hay artículos aún!</Typography>}
     </Container>
   )
