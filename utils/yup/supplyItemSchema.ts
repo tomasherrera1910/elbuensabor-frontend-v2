9b554deb5ee80e120378e5b7028832875a1402b5
tomasherrera1910 <tomymza10@gmail.com>
2023-05-14 14:26:47 -0300

    fix supplyItem schema

--- utils/yup/supplyItemSchema.ts
+++ utils/yup/supplyItemSchema.ts
@@ -1,13 +1,13 @@
 import * as yup from 'yup'
 
 const itemSupplySchema = yup.object().shape({
-  rubro: yup.string().required(),
-  denominacion: yup.string().required(),
-  precioCompra: yup.number().required(),
+  rubro: yup.string().required('Debe seleccionar un rubro'),
+  denominacion: yup.string().required('Coloque la denominación del producto'),
+  precioCompra: yup.number().required('Coloque el precio de compra del producto'),
   precioVenta: yup.number(),
-  stockActual: yup.number().required(),
+  stockActual: yup.number().required('Coloque el stock del producto'),
   stockMinimo: yup.number(),
-  unidadMedida: yup.string().required()
+  unidadMedida: yup.string().required('Debe seleccionar una unidad de medida')
 })
 
 export default itemSupplySchema
