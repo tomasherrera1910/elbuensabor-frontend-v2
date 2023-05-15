e81d5b988aa6d54ea31da77970dd9d44bc322505
tomasherrera1910 <tomymza10@gmail.com>
2023-05-15 10:26:25 -0300

    add function for update supplies

--- hooks/useSupplies.ts
+++ hooks/useSupplies.ts
@@ -3,10 +3,21 @@ import { useState } from 'react'
 
 const useSupplies = ({ initialSupplies }: { initialSupplies: ItemSupply[] }) => {
   const [supplies, setSupplies] = useState(initialSupplies)
+
   const addNewSupply = (newSupply: ItemSupply) => {
     setSupplies([...supplies, newSupply])
   }
-  return { supplies, addNewSupply }
+
+  const updateSupply = (supply: ItemSupply) => {
+    setSupplies(prevSupplies => {
+      const suppliesCopy: ItemSupply[] = window.structuredClone(prevSupplies)
+      const idxToUpdate = suppliesCopy.findIndex(s => s.id === supply.id)
+      suppliesCopy[idxToUpdate] = supply
+      return suppliesCopy
+    })
+  }
+
+  return { supplies, addNewSupply, updateSupply }
 }
 
 export default useSupplies
