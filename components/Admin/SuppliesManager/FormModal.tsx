ab6ab379fe3f5285e9a7bae46484c0b2bb511e49
tomasherrera1910 <tomymza10@gmail.com>
2023-05-18 12:27:06 -0300

    refactoring for reutilice that for update supplies

--- components/Admin/SuppliesManager/FormModal.tsx
+++ components/Admin/SuppliesManager/FormModal.tsx
@@ -6,10 +6,11 @@ import { type ItemSupply } from '@/utils/types'
 interface Props {
   open: boolean
   handleClose: () => void
-  addNewSupply: (newSupply: ItemSupply) => void
+  handleSupplies: (supply: ItemSupply) => void
   edit?: boolean
+  actualItem?: ItemSupply
 }
-export default function FormModal ({ open, handleClose, addNewSupply, edit }: Props) {
+export default function FormModal ({ open, handleClose, handleSupplies, edit, actualItem }: Props) {
   return (
     <Dialog open={open} onClose={handleClose} scroll='paper'>
       <IconButton sx={{ position: 'absolute', right: 0 }} size='large' onClick={handleClose}>
@@ -17,7 +18,7 @@ export default function FormModal ({ open, handleClose, addNewSupply, edit }: Pr
       </IconButton>
       <Box padding={4}>
         <Typography fontWeight={500} textAlign='center' marginBottom={2}>ARTÍCULO INSUMO</Typography>
-        <ItemSupplyForm addNewSupply={addNewSupply} edit={edit} />
+        <ItemSupplyForm handleSupplies={handleSupplies} edit={edit} actualItem={actualItem} />
       </Box>
     </Dialog>
   )
