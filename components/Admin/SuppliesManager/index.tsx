a25a3b8fa3bb7034e11bf3aa503b63810d69450e
tomasherrera1910 <tomymza10@gmail.com>
2023-05-20 19:17:08 -0300

    add button for back to admin panel

--- components/Admin/SuppliesManager/index.tsx
+++ components/Admin/SuppliesManager/index.tsx
@@ -2,9 +2,10 @@ import { ItemSupply } from '@/utils/types'
 import { Button, Container, Stack, Typography } from '@mui/material'
 import FormModal from './FormModal'
 import useToggle from '@/hooks/useToggle'
-import { Add } from '@mui/icons-material'
+import { Add, ArrowBackIos } from '@mui/icons-material'
 import useSupplies from '@/hooks/useSupplies'
 import SupplyCard from './SupplyCard'
+import Link from 'next/link'
 
 export default function SuppliesManager ({ initialSupplies }: { initialSupplies: ItemSupply[] }) {
   const [showModalForm, handleModalForm] = useToggle()
@@ -14,9 +15,16 @@ export default function SuppliesManager ({ initialSupplies }: { initialSupplies:
       <Typography variant='h5' component='h1' fontWeight='900' textAlign='center' marginBottom={4}>
         ARTÍCULOS INSUMOS
       </Typography>
-      <Button startIcon={<Add />} onClick={handleModalForm}>
-        Agregar Artículo
-      </Button>
+      <Stack flexDirection='row' justifyContent='space-between' alignItems='center'>
+        <Link href='/login'>
+          <Button startIcon={<ArrowBackIos />} size='small'>
+            Ir a Admin
+          </Button>
+        </Link>
+        <Button startIcon={<Add />} onClick={handleModalForm} sx={{ width: 'fit-content' }}>
+          Agregar Artículo
+        </Button>
+      </Stack>
       <FormModal open={showModalForm} handleSupplies={addNewSupply} handleClose={handleModalForm} />
       {!supplies?.length && <Typography textAlign='center'>No hay artículos aún!</Typography>}
       <Stack direction='row' gap={2} flexWrap='wrap' justifyContent='center'>
