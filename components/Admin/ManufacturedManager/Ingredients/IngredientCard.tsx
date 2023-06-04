30095595ad377efc2a893b359691ab14bbd72a08
tomasherrera1910 <tomymza10@gmail.com>
2023-06-04 11:14:53 -0300

    remove visually ingredient when you delete it

--- components/Admin/ManufacturedManager/Ingredients/IngredientCard.tsx
+++ components/Admin/ManufacturedManager/Ingredients/IngredientCard.tsx
@@ -4,11 +4,17 @@ import { Ingredient } from '@/utils/types'
 import { Delete } from '@mui/icons-material'
 import { IconButton, Stack, Typography } from '@mui/material'
 
-export default function IngredientCard ({ ingredient }: { ingredient: Ingredient }) {
+interface Props {
+  ingredient: Ingredient
+  removeIngredient: (id: string) => void
+}
+export default function IngredientCard ({ ingredient, removeIngredient }: Props) {
   const userInfo = useUserSession(state => state.userInfo)
   const deleteIngredient = () => {
     deleteInfo(`articulosManuDetalle/${ingredient.id}`, userInfo?.token)
-      .then(() => {})
+      .then(() => {
+        removeIngredient(ingredient.id)
+      })
       .catch(() => {})
   }
   return (
