22c707c5ef1af1a33ace0f8171a2529c75f3725f
tomasherrera1910 <tomymza10@gmail.com>
2023-06-07 17:27:41 -0300

    handleDelete dish

--- components/Admin/ManufacturedManager/DishCard.tsx
+++ components/Admin/ManufacturedManager/DishCard.tsx
@@ -2,8 +2,26 @@ import { ItemManufactured, ItemSupply } from '@/utils/types'
 import { Delete, Edit } from '@mui/icons-material'
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
 import Ingredients from './Ingredients'
+import { useUserSession } from '@/store/user'
+import { deleteInfo } from '@/utils/CRUDActions'
 
-export default function DishCard ({ dish, supplies }: { dish: ItemManufactured, supplies: ItemSupply[] }) {
+interface Props {
+  dish: ItemManufactured
+  supplies: ItemSupply[]
+  removeSupply: (id: string) => void
+}
+export default function DishCard ({ dish, supplies, removeSupply }: Props) {
+  const userInfo = useUserSession(state => state.userInfo)
+  const handleDelete = (id: string) => {
+    deleteInfo(`articulosManufacturados/${id}`, userInfo?.token)
+      .then((_response) => {
+        console.log('entro')
+        removeSupply(id)
+      })
+      .catch((error) => {
+        console.error('Error: ', error)
+      })
+  }
   return (
     <Card key={dish.id} sx={{ width: 300 }}>
       <CardMedia
@@ -23,7 +41,7 @@ export default function DishCard ({ dish, supplies }: { dish: ItemManufactured, 
       </CardContent>
       <CardActions>
         <Button size='small' variant='outlined' startIcon={<Edit />}>Editar</Button>
-        <Button size='small' color='error' variant='outlined' startIcon={<Delete />}>Eliminar</Button>
+        <Button size='small' color='error' variant='outlined' startIcon={<Delete />} onClick={() => { handleDelete(dish.id) }}>Eliminar</Button>
       </CardActions>
     </Card>
   )
