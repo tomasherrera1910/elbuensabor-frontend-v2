1a6e12e7b1dfab2b564ef22408c3cf3f3b14baa7
tomasherrera1910 <tomymza10@gmail.com>
2023-06-23 09:59:31 -0300

    refactor map

--- components/Menus/index.tsx
+++ components/Menus/index.tsx
@@ -1,6 +1,6 @@
 import { type ItemManufactured } from '@/utils/types'
-import { ShoppingCart } from '@mui/icons-material'
-import { Button, Card, CardActions, CardContent, CardMedia, Container, Stack, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { Container, Stack, useMediaQuery, useTheme } from '@mui/material'
+import MenuCard from './MenuCard'
 
 interface Props {
   items: ItemManufactured[]
@@ -13,25 +13,7 @@ export default function Menus ({ items }: Props) {
       <Container maxWidth={xlScreen ? 'lg' : 'md'}>
         <Stack flexDirection='row' gap={2}>
           {items.map(dish => (
-            <Card sx={{ maxWidth: 345 }} key={dish.id}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image={dish.imagen.url}
-                title={`${dish.denominacion} imagen`}
-              />
-              <CardContent>
-                <Typography gutterBottom variant='h5' component='div'>
-                  {dish.denominacion}
-                </Typography>
-                <Typography variant='body2' color='text.secondary'>
-                  {dish.rubro}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size='small' endIcon={<ShoppingCart />}>Agregar a Carrito</Button>
-                <Button size='small'>Learn More</Button>
-              </CardActions>
-            </Card>
+            <MenuCard key={dish.id} dish={dish} />
           ))}
         </Stack>
       </Container>
