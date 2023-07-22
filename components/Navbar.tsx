b757a1f1ae9a7b6ab02118e9974e844f6615b8ff
tomasherrera1910 <tomymza10@gmail.com>
2023-07-22 18:31:12 -0300

    notification in cart button with items in cart

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,12 @@ import Link from 'next/link'
 import Sidebar from './Sidebar'
 import { useState } from 'react'
 import Cart from './Cart'
+import { useCart } from '@/store/cart'
 
 export default function Navbar () {
   const userInfo = useUserSession(state => state.userInfo)
   const [open, isOpen] = useState(false)
+  const cart = useCart(state => state.cart)
   const handleSidebar = () => { isOpen(!open) }
   const [openCart, isOpenCart] = useState(false)
   const handleCart = () => { isOpenCart(!openCart) }
@@ -31,8 +33,9 @@ export default function Navbar () {
               <AccountCircle />
               <Typography>{userInfo?.username}</Typography>
             </Stack>
-            <IconButton color='inherit' onClick={handleCart}>
+            <IconButton color='inherit' onClick={handleCart} sx={{ position: 'relative' }}>
               <ShoppingCart />
+              {cart.length > 0 && <div style={{ position: 'absolute', bottom: 0, right: 0, width: '16px', height: '16px', borderRadius: '9999px', backgroundColor: 'red', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#fff', fontSize: '8px' }}>{cart.length}</div>}
             </IconButton>
           </Stack>
         </Toolbar>
