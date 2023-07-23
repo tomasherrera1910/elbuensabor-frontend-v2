b80496d554cc2f367cad55a25473da8eb1b0c16b
tomasherrera1910 <tomymza10@gmail.com>
2023-07-23 15:42:47 -0300

    add total price and more issues in cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,6 +1,6 @@
 import { useCart } from '@/store/cart'
 import { Cancel } from '@mui/icons-material'
-import { Card, CardContent, CardMedia, Drawer, IconButton, Typography, useTheme } from '@mui/material'
+import { Card, CardContent, Drawer, IconButton, Stack, Typography, useTheme } from '@mui/material'
 
 export default function Cart ({ open, handleClose }: { open: boolean, handleClose: () => void }) {
   const theme = useTheme()
@@ -10,19 +10,37 @@ export default function Cart ({ open, handleClose }: { open: boolean, handleClos
       <IconButton onClick={handleClose}>
         <Cancel />
       </IconButton>
-      {cart.length < 1 && <Typography>No hay productos aún en el carrito.</Typography>}
-      {cart.map(({ item, quantity }) => (
-        <Card key={item.denominacion}>
+      <Stack flexDirection='column' gap={2}>
+        {cart.length < 1 && <Typography>No hay productos aún en el carrito.</Typography>}
+        {cart.map(({ item, quantity }) => (
+          <Card key={item.denominacion}>
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                {item.denominacion}
+              </Typography>
+              <Stack>
+                <Typography variant='h6' color='text.secondary'>
+                  $ {item.precioVenta * quantity} x {quantity}
+                </Typography>
+              </Stack>
+            </CardContent>
+          </Card>
+        ))}
+        <Card>
           <CardContent>
-            <Typography gutterBottom variant='h5' component='div'>
-              {item.denominacion}
-            </Typography>
-            <Typography variant='h6' color='text.secondary'>
-              $ {item.precioVenta}
-            </Typography>
+            <Stack flexDirection='row' gap={2}>
+              <Typography gutterBottom variant='h5' component='div'>
+                TOTAL:
+              </Typography>
+              <Typography variant='h6' color='text.secondary'>
+                $ {cart.reduce((acc, current) => {
+                return acc + current.item.precioVenta * current.quantity
+              }, 0)}
+              </Typography>
+            </Stack>
           </CardContent>
         </Card>
-      ))}
+      </Stack>
     </Drawer>
   )
 }
