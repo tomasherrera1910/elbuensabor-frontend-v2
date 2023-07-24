ab824c25d5897b84fe225ee94a361e90c91269fe
tomasherrera1910 <tomymza10@gmail.com>
2023-07-24 10:18:52 -0300

    handle quantity since cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,10 +1,12 @@
 import { useCart } from '@/store/cart'
-import { Cancel } from '@mui/icons-material'
+import { Add, Cancel, Remove } from '@mui/icons-material'
 import { Card, CardContent, Drawer, IconButton, Stack, Typography, useTheme } from '@mui/material'
 
 export default function Cart ({ open, handleClose }: { open: boolean, handleClose: () => void }) {
   const theme = useTheme()
   const cart = useCart(state => state.cart)
+  const add = useCart(state => state.add)
+  const remove = useCart(state => state.remove)
   return (
     <Drawer variant='temporary' open={open} onClose={handleClose} anchor='right' PaperProps={{ sx: { backgroundColor: theme.palette.primary.main, color: '#fff', paddingTop: { md: 8 }, paddingX: 4 } }}>
       <IconButton onClick={handleClose}>
@@ -12,16 +14,35 @@ export default function Cart ({ open, handleClose }: { open: boolean, handleClos
       </IconButton>
       <Stack flexDirection='column' gap={2}>
         {cart.length < 1 && <Typography>No hay productos aún en el carrito.</Typography>}
-        {cart.map(({ item, quantity }) => (
+        {cart.map(({ item, quantity }, idx) => (
           <Card key={item.denominacion}>
             <CardContent>
               <Typography gutterBottom variant='h5' component='div'>
                 {item.denominacion}
               </Typography>
-              <Stack>
+              <Stack direction='row' alignItems='center' justifyContent='space-around'>
                 <Typography variant='h6' color='text.secondary'>
                   $ {item.precioVenta * quantity} x {quantity}
                 </Typography>
+                <Stack direction='row' alignItems='center' justifyContent='center' sx={{ padding: 0 }}>
+                  <IconButton
+                    onClick={() => {
+                      remove(idx)
+                    }}
+                    disabled={quantity === 1}
+                  >
+                    <Remove />
+                  </IconButton>
+                  <Typography color='GrayText'>
+                    {quantity ?? 1}
+                  </Typography>
+                  <IconButton onClick={() => {
+                    add(idx)
+                  }}
+                  >
+                    <Add />
+                  </IconButton>
+                </Stack>
               </Stack>
             </CardContent>
           </Card>
