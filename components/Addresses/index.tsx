347f6d08a30d8a86755f4e39344d5e404aab9430
tomasherrera1910 <tomymza10@gmail.com>
2023-08-06 12:05:23 -0300

    display addresses info

--- components/Addresses/index.tsx
+++ components/Addresses/index.tsx
@@ -1,11 +1,34 @@
-import { Container, useMediaQuery, useTheme } from '@mui/material'
+import { useUserSession } from '@/store/user'
+import { getInfo } from '@/utils/CRUDActions'
+import { type Address } from '@/utils/types'
+import { CircularProgress, Container, Stack, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 export default function Addresses () {
   const theme = useTheme()
   const xlScreen = useMediaQuery(theme.breakpoints.up('xl'))
+  const userInfo = useUserSession(state => state.userInfo)
+  const [addresses, setAddresses] = useState<Address[]>([])
+  const [loading, isLoading] = useState(false)
+  useEffect(() => {
+    isLoading(true)
+    getInfo(`addresses/${userInfo?.id ?? ''}`)
+      .then(setAddresses)
+      .catch((e) => { console.error(e) })
+      .finally(() => { isLoading(false) })
+  }, [userInfo])
   return (
     <Container maxWidth={xlScreen ? 'lg' : 'md'}>
-      Direcciones
+      <Stack flexDirection='column'>
+        Direcciones
+        {loading && <CircularProgress />}
+        {!loading && addresses.length < 1 && <Typography>No hay direcciones asociadas a este usuario.</Typography>}
+        {addresses.length > 1 && addresses.map((address) => (
+          <Typography key={address.id}>
+            {address.calle} - {String(address.numero)} ({address.localidad})
+          </Typography>
+        ))}
+      </Stack>
     </Container>
   )
 }
--- pages/direcciones.tsx
+++ pages/direcciones.tsx
@@ -2,20 +2,20 @@ import CustomHead from '@/components/CustomHead'
 import useUser from '@/hooks/useUser'
 import Layout from '@/components/Layout'
 import Addresses from '@/components/Addresses'
-import { Address } from '@/utils/types'
-import { GetServerSideProps } from 'next'
-import { getInfo } from '@/utils/CRUDActions'
-interface Props {
-  addresses: Address[]
-}
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const addresses: Address[] = await getInfo('addresses')
-  return {
-    props: {
-      addresses
-    }
-  }
-}
+// import { Address } from '@/utils/types'
+// import { GetServerSideProps } from 'next'
+// import { getInfo } from '@/utils/CRUDActions'
+// interface Props {
+//   addresses: Address[]
+// }
+// export const getServerSideProps: GetServerSideProps<Props> = async () => {
+//   const addresses: Address[] = await getInfo('addresses')
+//   return {
+//     props: {
+//       addresses
+//     }
+//   }
+// }
 export default function Direcciones () {
   const { user, loading } = useUser()
   const headTitle = loading ? 'Cargando' : user ? 'Direcciones' : 'Login'
